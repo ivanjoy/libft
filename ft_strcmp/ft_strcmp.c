0db71197c946e75f273649f05e433a98bdd00b04
Ivan Joy <ivanejoy@gmail.com>
2017-05-31 18:46:48 -0700

    fixed a small formatting issue

--- ft_strcmp/ft_strcmp.c
+++ ft_strcmp/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: ijoy <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/31 17:58:26 by ijoy              #+#    #+#             */
-/*   Updated: 2017/05/31 18:02:20 by ijoy             ###   ########.fr       */
+/*   Updated: 2017/05/31 18:44:36 by ijoy             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int		ft_strcmp(const char *s1, const char *s2)
 	while (*s1 && *s2)
 	{
 		if (*s1 != *s2)
-			return (*s1 - *s2);
+			return ((unsigned char)(*s1) - (unsigned char)(*s2));
 		s1++;
 		s2++;
 	}
--- ft_strncmp/ft_strncmp.c
+++ ft_strncmp/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: ijoy <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/31 18:13:01 by ijoy              #+#    #+#             */
-/*   Updated: 2017/05/31 18:17:21 by ijoy             ###   ########.fr       */
+/*   Updated: 2017/05/31 18:46:11 by ijoy             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 	while (*s1 && *s2 && i < n)
 	{
 		if (*s1 != *s2)
-			return (*s1 - *s2);
+			return ((unsigned char)(*s1) - (unsigned char)(*s2));
 		s1++;
 		s2++;
 		i++;
